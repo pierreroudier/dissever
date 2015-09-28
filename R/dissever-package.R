7c967e6988c69cbc3b7e8ab10f7f5233031c04c5
Pierre Roudier <pierre.roudier@gmail.com>
2015-09-28 17:34:14 +1300

    Initial commit

---
+++ R/dissever-package.R
@@ -0,0 +1,50 @@
+#'@title dissever
+#'@name dissever-package
+#'@description Dissever: algorithm for spatial downscaling
+#'@details Algorithm described in: [DOI: 10.1016/j.cageo.2011.08.021]
+#'@author Brendan Malone (brendan.malone@@sydney.edu.au);
+NULL
+
+#' @name edgeroi_covariates
+#' @docType data
+#' @aliases edgeroi-covariates, edgeGrids
+#' @title Selected subset of environmental covariates for the Edgeroi District, NSW
+#' @description A \code{RasterStack} of selected environmental covariates for a small area of the Edgeroi district in NSW, Australia.
+#' @usage data(edgeGrids)
+#' @format \code{edgeGrids} is an 87 row, 117 column, 5 layer \code{RasterStack} of selected environmental covariates from a small area in the Edgeroi district NSW, Australia. The grids have a pixel resolution of 90m x 90m. It contains the following layers:
+#' \describe{
+#'      \item{\code{Doserate}}{numeric; gamma radiometric data}
+#'      \item{\code{Elevation}}{numeric; topographic variable of bare earth ground elevation. Derived from digital elevation model}
+#'      \item{\code{Panchromat}}{numeric; panchromatic band of the Landsat 7 satelite}
+#'      \item{\code{Slope}}{numeric; Slope gradient of the land surface. Derived from digital elevation model }
+#'      \item{\code{TWI}}{numeric; topographic wetness index. Secondary derivative of the digital elevation model }
+#'    }
+#' @details The Edgeroi District, NSW is an intensive cropping area upon the fertile alluvial Namoi River plain. The District has been the subject of many soil invetigations, namely McGarry et al. (1989) whom describe an extensive soil data set collected from the area. More recently, digital soil mapping studies of the area have been conducted, for example, Malone et al. (2009).
+#' @note The raw spatial data that contributed to the creation of \code{edgeGrids} were sourced from publically accessable repositories hosted by various Australian Government and international agencies including CSIRO (for the DEM), Geosciences Australia (for the radiometric data) and NASA (for the Landsat 7 ETM+ data). The projection for the \code{RasterStack} is WGS 84 Zone 55.
+#' @references
+#' \itemize{
+#'    \item Malone, B.P., McBratney, A.B., Minasny, B. (2009) \href{http://dx.doi.org/10.1016/j.geoderma.2009.10.007}{Mapping continuous depth functions of soil carbon storage and available water capacity}. Geoderma 154, 138-152.
+#'    \item McGarry, D., Ward, W.T., McBratney, A.B. (1989) Soil Studies in the Lower Namoi Valley: Methods and Data. The Edgeroi Data Set. (2 vols) (CSIRO Division of Soils: Adelaide).
+#'  }
+#'
+NULL
+
+#' @name edgeroi_carbon
+#' @docType data
+#' @aliases edgeroi SOC 1km, target
+#' @title Subset of the 1-km resolution soil carbon map of the Edgeroi District, NSW
+#' @description A \code{RasterLayer} of a subset of the 1km SOC map for the Edgeroi District, NSW. It portrays soil carbon stock for the 0-30cm depth interval only.
+#' @usage data(edgeTarget_C)
+#' @format \code{edgeTarget_C} is an 7 row, 10 column \code{RasterLayer} of soil carbon stock for a small area in the Edgeroi district NSW, Australia. The grid has a pixel resolution of 1km x 1km. It contains the layer:
+#' \describe{
+#'      \item{\code{target}}{numeric; predicted soil carbon stock for the 0-30cm depth interval}
+#' }
+#' @details The Edgeroi District, NSW is an intensive cropping area upon the fertile alluvial Namoi River plain. The District has been the subject of many soil invetigations, namely McGarry et al. (1989) whom describe an extensive soil data set collected from the area. More recently, digital soil mapping studies of the area have been conducted, for example, Malone et al. (2009). The 1km mapping of soil carbon stock was performed using digital soil mapping methods McBratney et al. (2003) using the soil data from McGarry et al. (1989) and available environmental covariates.
+#' @note The projection for the \code{RasterLayer} is WGS 84 Zone 55. It has the same mapping extent i.e. of the exact same area as the \code{edgeGrids} dataset provided in this package, but a different resolution.
+#' @references
+#'   \itemize{
+#'    \item Malone, B.P., McBratney, A.B., Minasny, B. (2009) \href{http://dx.doi.org/10.1016/j.geoderma.2009.10.007}{Mapping continuous depth functions of soil carbon storage and available water capacity}. Geoderma 154, 138-152.
+#'    \item McBratney, A.B., Mendonca Santos, M.L., Minasny, B. (2003) \href{http://dx.doi.org/10.1016/S0016-7061(03)00223-4}{On Digtial Soil mapping}. Geoderma 117: 3-52.
+#'    \item McGarry, D., Ward, W.T., McBratney, A.B. (1989) Soil Studies in the Lower Namoi Valley: Methods and Data. The Edgeroi Data Set. (2 vols) (CSIRO Division of Soils: Adelaide).
+#'  }
+NULL
---
+++ R/dissever.R
@@ -0,0 +1,427 @@
+# library(raster)
+# library(plyr)
+# library(dplyr)
+# library(caret)
+# library(boot)
+# library(viridis)
+
+# simple wrapper around raster::as.data.frame that
+# handles categorical data columns correctly
+.as_data_frame_factors <- function(x, ...) {
+
+  # Get data.frame
+  res <- as.data.frame(x, ...)
+
+  # We only need to do something if
+  # there is categorical data in the stack
+  if (any(is.factor(x))) {
+
+    # Get names of original stack
+    # (not affected by bug)
+    nm <- names(x)
+
+    # Test if coordinates have been returned
+    args <- list(...)
+    if("xy" %in% names(args)) {
+      xy <- args[['xy']]
+    } else {
+      xy <- FALSE
+    }
+
+    # If coordinates asked to be returned, we need
+    # to take this into account
+    if (xy) {
+      names(res)[-1*1:2] <- nm
+    } else {
+      names(res) <- nm
+    }
+
+  }
+
+  res
+}
+
+.join_interpol <- function(coarse_df, fine_df, attr, by = 'cell'){
+  # Nearest-neighbour interpolation as an inner SQL join
+  inner_join(fine_df, coarse_df , by = by) %>%
+    select(matches(attr))
+}
+
+.create_lut_fine <- function(coarse, fine) {
+  extract(coarse, coordinates(fine))
+}
+
+.default_control_init <- caret::trainControl(
+  method = 'cv',
+  number = 5#,
+  #verboseIter = TRUE
+)
+
+# fits one model to the entire training set
+.default_control_iter <- caret::trainControl(method = 'none')
+
+.create_tune_grid <- function(model, tune_length) {
+  params <- modelLookup(model)$parameter
+  grid <- expand.grid(lapply(1:length(params), function(x) 1:tune_length))
+  names(grid) <- as.character(params)
+  grid
+}
+
+# Computes the regression model between some coarse data and
+# the stack of covariates
+.update_model <- function(x, y, method = 'rf', control, tune_grid){
+  # Picks the parameters of the model using RMSE on the first run
+  # Then use the optimised parameters in the iteration loop to save
+  # on computing time
+  # Basically we just need to change the trainControl object to do that
+
+  fit <- train(
+    x = x,
+    y = y, # in this case train needs a vector
+    method = method,
+    trControl = control,
+    tuneGrid  = tune_grid
+  )
+
+  fit
+}
+
+.has_parallel_backend <- function() getDoParWorkers() > 1
+
+.get_split_idx <- function(n, p) sort(1:n %% p)
+
+# Generates prediction intervals using bootstraping
+#
+.bootstrap_ci <- function(fit, fine_df, level = 0.95, n = 50L) {
+
+  # training data
+  df <- fit$trainingData
+
+  # Regression method
+  reg_method <- fit$method
+
+  # Bootstrap
+  boot_samples <- boot(df, function(data, idx, method = reg_method) {
+
+    bootstrap_df <- data[idx, ]
+
+    bootstrap_fit <- train(
+      .outcome ~ .,
+      data = bootstrap_df,
+      method = method,
+      trControl = trainControl(method = "none"),
+      tuneGrid = fit$bestTune)
+
+    # generate predictions
+    predict(bootstrap_fit, fine_df)
+
+  }, n)
+
+  # generate CI estimates + mean
+  ci <- c((1 - level) / 2, 1 - (1 - level) / 2)
+  res <- data.frame(
+    lower = aaply(boot_samples$t, 2, quantile, probs = ci[1]),
+    mean = aaply(boot_samples$t, 2, mean),
+    upper = aaply(boot_samples$t, 2, quantile, probs = ci[2])
+  )
+
+  res
+}
+
+.predict_map <- function(fit, data, split = NULL, boot = NULL) {
+  if (.has_parallel_backend()) {
+    # Get number of registered workers
+    n_workers <- length(unique(split))
+
+    # The split vector is assigning a cmputing core to
+    # each sample in `data`
+    if (n_workers < 1) stop('Wrong split vector')
+
+    # Parallel prediction
+    res <- foreach(
+      i = 0:(n_workers - 1),
+      .combine = c,
+      .packages = 'caret'
+    ) %dopar% {
+      if (is.null(boot)) {
+        predict(fit, newdata = data[split == i, ])
+      } else {
+        # Use bootstraping to get confidence intervals
+        .bootstrap_ci(fit = fit, fine_df = data[split == i, ], level = 0.95, n = boot)
+      }
+    }
+  } else {
+    if (is.null(boot)) {
+      res <- predict(fit, data)
+    } else {
+      # Use bootstraping to get confidence intervals
+      res <- .bootstrap_ci(fit = fit, fine_df = data, level = 0.95, n = boot)
+    }
+  }
+
+  res
+}
+
+.dissever <- function(
+    coarse,
+    fine,
+    method = "rf",
+    p = 0.5,
+    thresh = 0.01,
+    min_iter = 5,
+    max_iter = 20,
+    boot = NULL,
+    tune_length = 3,
+    tune_grid = .create_tune_grid(model = method, tune_length = tune_length),
+    train_control_init = .default_control_init,
+    train_control_iter = .default_control_iter,
+    verbose = FALSE
+  ){
+
+  # Stop if resolution of covariates is not higher than resolution of coarse data
+  if (min(res(fine)) >= min(res(coarse))) {
+    stop('Resolution of fine data should be higer than resolution of coarse data')
+  }
+
+  # Store names of coarse data and fine-scale covariates
+  nm_coarse <- names(coarse)
+  nm_covariates <- names(fine)
+
+  # Get cell numbers of the coarse grid
+  ids_coarse <- raster(coarse)
+  ids_coarse[] <- 1:ncell(coarse)
+  names(ids_coarse) <- 'cell'
+
+  # Convert coarse data to data.frame
+  coarse_df <- na.exclude(.as_data_frame_factors(coarse, xy = TRUE))
+  coarse_df$cell <- 1:nrow(coarse_df) # integer
+
+  # Convert fine data to data.frame
+  fine_df <- .as_data_frame_factors(fine, xy = TRUE)
+  # Add coarse cell ID to fine data.frame
+  fine_df[['cell']] <- as.integer(.create_lut_fine(ids_coarse, fine))
+  # Remove NA values
+  fine_df <- na.exclude(fine_df)
+
+  # Resampled national model onto fine grid
+  fine_df <- cbind(
+    fine_df,
+    .join_interpol(coarse_df = coarse_df[, c('cell', nm_coarse)], fine_df = fine_df, attr = nm_coarse, by = 'cell')
+  )
+
+  # Sub-sample for modelling
+  n_spl <- ceiling(nrow(fine_df) * p) # Number of cells to sample
+  id_spl <- sample(1:nrow(fine_df), size = n_spl) # sample random grid cells
+
+  # Compute initial model
+  if (verbose) message('Selecting best model parameters')
+
+  fit <- .update_model(
+    x = fine_df[id_spl, nm_covariates],
+    y = fine_df[id_spl, nm_coarse, drop = TRUE],
+    method = method,
+    control = train_control_init,
+    tune_grid = tune_grid
+  )
+
+  # Getting best setof params
+  best_params <- fit$bestTune
+
+  if (verbose) {
+    best_params_str <- paste(
+      lapply(names(best_params), function(x) paste(x, " = ", best_params[[x]], sep = "")),
+      collapse = ", ")
+    message("Parameters retained: ", best_params_str)
+  }
+
+  # Initiate matrix to store performance of disseveration
+  perf <- matrix(ncol = 3, nrow = 0, dimnames = list(NULL,c("lower_rmse", "rmse", "upper_rmse")))
+
+  # Initiate dissever result data.frame
+  diss_result <- fine_df[, c('x', 'y', 'cell', nm_coarse)]
+  # Our first approximation is actually the nearest neighbour interpolation
+  diss_result$diss <- fine_df[[nm_coarse]]
+
+  # Initiate dissever results data.frame aggregated back to coarse grid
+  diss_coarse <- coarse_df
+  diss_coarse$diss <- coarse_df[[nm_coarse]]
+
+  # Initialising best model selection
+  best_fit <- Inf
+
+  # If parallel computing: Get split vector, assigning each row in fine data
+  # to a core (for prediction of models on fine grid)
+  if (.has_parallel_backend()) {
+    n_cores <- getDoParWorkers()
+    split_cores <- .get_split_idx(nrow(fine_df), p = n_cores)
+  } else {
+    split_cores <- NULL
+  }
+
+  for (k in 1:max_iter){
+
+    if (verbose) message('| - iteration ', k)
+    # if (verbose) message('| -- computing adjustement factor')
+
+    # Calculate adjustment factor
+    diss_coarse$adjust <- diss_coarse[[nm_coarse]] / diss_coarse[['diss']]
+
+    # Resample adjustement factor to fine grid
+    diss_result$adjust <- .join_interpol(diss_coarse, fine_df, attr = 'adjust', by = 'cell')[, 'adjust']
+
+    # Apply adjustement and replace the current
+    diss_result$diss <- diss_result$adjust * diss_result$diss
+
+    # Update model
+    if (verbose) message('| -- updating model')
+
+    # Sampling points
+    # id_spl <- sample(1:nrow(fine_df), size = n_spl)
+
+    fit <- .update_model(
+      x = fine_df[id_spl, nm_covariates],
+      y = diss_result[id_spl, 'diss', drop = TRUE],
+      method = method,
+      control = train_control_iter,
+      tune_grid = best_params
+    )
+
+    if (verbose) message('| -- updating predictions')
+
+    # Update dissever predictions on fine grid
+    diss_result$diss <- .predict_map(fit, fine_df, split = split_cores)
+
+    # if (verbose) message('| -- averaging prediction on coarse grid')
+
+    # Aggregate average prediction on coarse grid
+    diss_coarse <- diss_result %>%
+      group_by(cell) %>%
+      summarise(diss = mean(diss)) %>%
+      inner_join(coarse_df, ., by = "cell")
+
+    # if (verbose) message('| -- computing performance stats')
+
+    # RMSE
+    n <- nrow(diss_coarse)
+    sqe <- (diss_coarse[[nm_coarse]] - diss_coarse[['diss']])^2
+    mse <- mean(sqe)
+    rmse <- sqrt(mse) # RMSE
+
+    # Confidence intervals
+    # In this case we use 5% C.I.
+    t_student <- qt(1 - (0.05/2), df = n - 1) # 0.975 quantile from Student t distribution
+    var <- ((1)^2/(n * (n - 1))) * sum(sqe) # Variance
+    se <- sqrt(var) # Standard error
+    ci <- se * t_student
+    upper_ci <- mse + ci
+    lower_ci <- max(0, mse - ci)
+
+    # Fill result matrix
+    perf <- rbind(perf, c(sqrt(lower_ci), rmse, sqrt(upper_ci)))
+
+    if (verbose) message("| -- RMSE = ", round(rmse, 3))
+
+    # Choose whether we retain the model or not
+    if (rmse < best_fit){
+      best_model <- fit
+      best_fit <- rmse
+      best_iteration <- k
+    }
+
+    # We only test improvement if more than 5 iterations
+    if (k >= min_iter) {
+
+      # Computing stop criterion
+      stop_criterion <- mean(
+        (perf[k - 2, 2] - perf[k - 1, 2]) + # improvement at last iteration
+          (perf[k - 1, 2] - rmse) + # improvement at this iteration
+          (perf[k - 2, 2] - rmse) # improvement over last 2 iterations
+      )
+
+      # If we have reach some kind of pseudo-optimium,
+      # we finish iteration stage
+      if (stop_criterion <= thresh) break
+    }
+  }
+
+  if (verbose) message('Retaining model fitted at iteration ', best_iteration)
+
+  # Create Raster result
+  map <- rasterFromXYZ(
+    data.frame(
+      diss_result[, c('x', 'y')],
+      diss = .predict_map(best_model, fine_df, split = split_cores, boot = boot)
+    ),
+    res = res(fine),
+    crs = projection(fine)
+  )
+
+  res <- list(
+    fit = fit,
+    map = map,
+    perf = data.frame(perf)
+  )
+
+  class(res) <- c(class(res), 'dissever')
+
+  return(res)
+}
+
+plot.dissever <- function(x, type = 'map', ...) {
+
+  if (! type %in% c('map', 'perf')) stop('Invalid type of plot.')
+
+  if (type == 'map') {
+    plot(x$map, col = viridis(100), ...)
+  } else {
+    # Get number of iterations
+    n_iter <- nrow(x$perf)
+    plot(1:n_iter, x$perf$rmse, type = 'l', xlab = 'Iteration', ylab = 'RMSE', ylim = range(x$perf), ...)
+    lines(1:n_iter, x$perf$upper_rmse, lty = 3)
+    lines(1:n_iter, x$perf$lower_rmse, lty = 3)
+    # Get selected model
+    best <- which.min(x$perf$rmse)
+    points(best, x$perf[best, 'rmse'], pch = 16, col = 2)
+  }
+}
+
+print.dissever <- function(x, ...) {
+  print(x$fit, ...)
+}
+
+summary.dissever <- function(x, ...) {
+  summary(x$fit, ...)
+}
+
+if(!isGeneric("dissever")) {
+  setGeneric("dissever", function(coarse, fine, ...) {
+    standardGeneric("dissever")
+  })
+}
+
+#' @title Spatial downscaling
+#' @name dissever
+#' @aliases dissever,RasterLayer,RasterStack-method
+#' @description Performs spatial downscaling of coarse grid mapping to fine grid mapping using predictive covariates and a model fitted using the caret package.
+#' @param coarse object of class \code{"RasterLayer"}, the coarse-resolution layer that needs to be downscaled
+#' @param fine object of class \code{"RasterStack"}, the fine-resolution stack of predictive covariates
+#' @param method a string specifying which classification or regression model to use (via the caret package). Possible values are found using names(caret::getModelInfo()).
+#' @param p numeric, proportion of the coarse map that is sampled for fitting the dissever model (between 0 and 1, defaults to 0.5)
+#' @param thresh numeric, dissever iterations will proceed until the RMSE of the dissever model reaches this value, or until the maximum number of iterations is met (defaults to 0.01)
+#' @param min_iter numeric, minimum number of iterations (defaults to 5)
+#' @param max_iter numeric, maximum number of iterations (defaults to 20)
+#' @param  boot numeric, if not NULL (default), the number of bootstrap replicates used to derive the confidence intervals
+#' @param tune_length numeric, the number of parameters to test to find the optimal parametrisation of the caret model (defaults to 3)
+#' @param tune_grid a data frame with possible tuning values
+#' @param train_control_init Control parameters for finding the optimal parameters of the caret model (see trainControl)
+#' @param train_control_iter Control parameters for fitting the caret model during the iteration phase (see trainControl)
+#' @param verbose controls the verbosity of the output (TRUE or FALSE)
+#' @docType methods
+#' @author Brendan Malone, Pierre Roudier
+#' @references Malone, B.P, McBratney, A.B., Minasny, B., Wheeler, I., (2011) A general method for downscaling earth resource information. Computers & Geosciences, 41: 119-125. \url{http://dx.doi.org/10.1016/j.cageo.2011.08.021}
+#'
+setMethod(
+  'dissever',
+  signature(coarse = "RasterLayer", fine = "RasterStack"),
+  .dissever
+)
